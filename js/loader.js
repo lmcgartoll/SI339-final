49f47dd974a616c1e398e3f5da35abc693fd3d83
lmcgartoll <112490051+lmcgartoll@users.noreply.github.com>
2022-12-06 23:00:06 -0500

    Delete js directory

--- js/loader.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", () => {
-  const loader = document.querySelector(".loader_wrapper");
-  loader.classList.add("loader--hidden");
-
-  //   loader.addEventListener("transitionend", () => {
-  //     document.body.removeChild(loader);
-  //   });
-});
